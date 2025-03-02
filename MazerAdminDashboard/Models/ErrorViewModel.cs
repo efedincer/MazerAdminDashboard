1070a069d811c0785d76c711fa80fd58d20e7e24
efedincer <dincer.efe@gmail.com>
2025-03-02 16:10:48 +0300

    Add project files.

---
+++ MazerAdminDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MazerAdminDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
