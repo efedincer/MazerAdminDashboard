0c214d4df3f8b8504007a3f2e0d635a1840b2592
efedincer <dincer.efe@gmail.com>
2025-03-02 16:42:13 +0300

    assets added

--- MazerAdminDashboard/Controllers/HomeController.cs
+++ MazerAdminDashboard/Controllers/HomeController.cs
@@ -2,31 +2,50 @@ using System.Diagnostics;
 using MazerAdminDashboard.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MazerAdminDashboard.Controllers
+namespace MazerAdminDashboard.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult LayoutDefault()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    public IActionResult LayoutHorizontal()
+    {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    public IActionResult LayoutVertical1Column()
+    {
+        return View();
+    }
+
+    public IActionResult LayoutVerticalNavbar()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
